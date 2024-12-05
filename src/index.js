1bbac3cdc536b68ae8018322b6c6364c34cda3c3
Bhavesh Kadachha <151392003+kadachhabhavesh@users.noreply.github.com>
2024-12-05 19:59:21 +0530

    add toast

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Demo from './Demo';
+import { Toaster } from 'react-hot-toast';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Toaster position="top-center" reverseOrder={false} />
     <App />
   </React.StrictMode>
 );
